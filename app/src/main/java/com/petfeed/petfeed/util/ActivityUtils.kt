4afc7c59f5021cb9fe7be9b6cf4c68233d313e5d
skdlsco <nicnic1234@naver.com>
2018-08-11 20:29:08 +0900

    mod ActivityUtils

--- app/src/main/java/com/petfeed/petfeed/util/ActivityUtils.kt
+++ app/src/main/java/com/petfeed/petfeed/util/ActivityUtils.kt
@@ -9,11 +9,14 @@ import android.view.WindowManager
 
 object ActivityUtils {
     fun statusBarSetting(window: Window, context: Context? = null, colorId: Int = 0, iconWhite: Boolean = false) {
-        window.run {
-            if (iconWhite)
-                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.run {
+                decorView.systemUiVisibility =
+                        if (iconWhite)
+                            View.SYSTEM_UI_FLAG_VISIBLE
+                        else
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
                 addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
                 context?.let {
                     if (colorId != 0)
