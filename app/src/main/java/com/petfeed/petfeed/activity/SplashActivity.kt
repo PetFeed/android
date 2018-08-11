0a9b6fa5da59009ca7811f2c3ee8fd4bdef1f982
skdlsco <nicnic1234@naver.com>
2018-08-11 20:18:48 +0900

    mod ui

--- app/src/main/java/com/petfeed/petfeed/activity/SplashActivity.kt
+++ app/src/main/java/com/petfeed/petfeed/activity/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.petfeed.petfeed.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.petfeed.petfeed.R
+import com.petfeed.petfeed.util.ActivityUtils
 import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -10,8 +11,8 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        ActivityUtils.statusBarSetting(window, this, R.color.brown1, true)
         startActivity<LoginActivity>()
-        finish()
+//        finish()
     }
 }
--- app/src/main/java/com/petfeed/petfeed/util/ActivityUtils.kt
+++ app/src/main/java/com/petfeed/petfeed/util/ActivityUtils.kt
@@ -3,12 +3,15 @@ package com.petfeed.petfeed.util
 import android.content.Context
 import android.os.Build
 import android.support.v4.content.ContextCompat
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 
 object ActivityUtils {
-    fun statusBarSetting(window: Window, context: Context? = null, colorId: Int = 0) {
+    fun statusBarSetting(window: Window, context: Context? = null, colorId: Int = 0, iconWhite: Boolean = false) {
         window.run {
+            if (iconWhite)
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
             clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
