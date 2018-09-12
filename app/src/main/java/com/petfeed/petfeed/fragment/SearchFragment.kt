771a20a62789326cc3b9e2536d1e445e812e8f00
skdlsco <nicnic4102@gmail.com>
2018-09-12 22:07:58 +0900

    fix search keyboard hide

--- app/src/main/java/com/petfeed/petfeed/fragment/SearchFragment.kt
+++ app/src/main/java/com/petfeed/petfeed/fragment/SearchFragment.kt
@@ -3,14 +3,22 @@ package com.petfeed.petfeed.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.NestedScrollView
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.nitrico.lastadapter.LastAdapter
 import com.petfeed.petfeed.BR
 import com.petfeed.petfeed.R
+import com.petfeed.petfeed.activity.DetailFeedActivity
+import com.petfeed.petfeed.activity.MainActivity
+import com.petfeed.petfeed.databinding.ItemBoardBinding
 import kotlinx.android.synthetic.main.fragment_search.*
+import org.jetbrains.anko.childrenRecursiveSequence
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.support.v4.startActivity
 import java.util.*
 
 class SearchFragment : Fragment() {
@@ -33,12 +41,39 @@ class SearchFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setRecyclerView()
+        scrollView.childrenRecursiveSequence().forEach {
+            if (it.id == R.id.boardRecyclerView) {
+                return@forEach
+            }
+            it.onClick { _ ->
+                hideKeyBoard()
+            }
+        }
+        scrollView.setOnScrollChangeListener { _: NestedScrollView?, _: Int, _: Int, _: Int, _: Int ->
+            Log.e("Asdfasdf", "Asdf")
+            hideKeyBoard()
+        }
+
     }
 
-    fun setRecyclerView() {
+    private fun hideKeyBoard() {
+        (this@SearchFragment.context as MainActivity).keyboardHelper.run {
+            if (isKeyboardVisible()) {
+                hideKeyboard()
+            }
+        }
+    }
+
+    private fun setRecyclerView() {
         boardRecyclerView.run {
             LastAdapter(boards, BR.item)
-                    .map<String>(R.layout.item_board)
+                    .map<String, ItemBoardBinding>(R.layout.item_board) {
+                        onClick {
+                            it.itemView.onClick {
+                                startActivity<DetailFeedActivity>()
+                            }
+                        }
+                    }
                     .into(this)
             layoutManager = LinearLayoutManager(context)
         }
